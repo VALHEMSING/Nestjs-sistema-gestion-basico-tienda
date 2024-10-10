a19f7005569cbbe3c2e5bc90c3ad2509ef06b858
YousyCamila <166034381+YousyCamila@users.noreply.github.com>
2024-10-09 20:42:26 -0500

    feat(back-end): Agregamos schema Cliente

---
+++ src/module/clientes/schema/clientes.schema.ts
@@ -0,0 +1,22 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import { IClientes } from '../interface/clientes.inteface';
+
+@Schema()
+export class Clientes extends Document implements IClientes{
+    
+    @Prop ({require: true})
+    numero_identificacion: string;
+
+    @Prop ({required: true})
+    nombre_cliente: string;
+
+    @Prop ({required: true})
+    email_cliente: string;
+
+    @Prop ({required: true})
+    celular_cliente: string;
+
+}
+
+export const ClientesSchema = SchemaFactory.createForClass(Clientes);
