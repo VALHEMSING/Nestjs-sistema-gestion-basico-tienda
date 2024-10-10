690e2189ebea8e68f33a759fffcb2a1094f8cb61
YousyCamila <166034381+YousyCamila@users.noreply.github.com>
2024-10-10 15:28:06 -0500

    feat(back-end): Agregamos la documentacion de swagger, totalmente funcionale

--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,16 @@ async function bootstrap() {
 
    // Configuración de Swagger
   const config = new DocumentBuilder()
-  .setTitle('API de Gestión de Inventario')
-  .setDescription('APIs.')
+  .setTitle('API Sistema de gestión ')
+  .setDescription('Se vera las APIs de nuestro sistema')
   .setVersion('1.0.0')
    .addTag('' )  // Etiqueta para agrupar los endpoints
   .build();
   const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api', app, document);  // Configura Swagger en el endpoint '/api'
+
+    console.log(`Documentación de Swagger está disponible en: http://localhost:3000/api`);
+
 
   await app.listen(3000);
 }
